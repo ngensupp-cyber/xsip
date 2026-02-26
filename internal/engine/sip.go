5021204ef3d82349d6b2643a0eaed32c10ef7090
AbuAlqasm <your_email@example.com>
2026-02-26 23:40:13 +0300

    Initial commit

--- internal/engine/sip.go
+++ internal/engine/sip.go
@@ -3,6 +3,7 @@ package engine
 import (
 	"context"
 	"log"
+	"time"
 	"nextgen-sip/internal/router"
 	"nextgen-sip/internal/firewall"
 	"nextgen-sip/pkg/utils"
@@ -10,6 +11,7 @@ import (
 	"github.com/emiago/sipgo"
 	"github.com/emiago/sipgo/sip"
 )
+
 
 type SIPEngine struct {
 	server *sipgo.Server
@@ -67,13 +69,50 @@ func (e *SIPEngine) onInvite(req *sip.Request, tx sip.ServerTransaction) {
 		return
 	}
 	
-	// 2. Start Call Tracking
+	// 2. Forward INVITE to Destination
+	log.Printf("[SIP] Proxying INVITE to %s", dest)
+	
+	destAddr, err := sip.ParseAddrUri(dest)
+	if err != nil {
+		log.Printf("[SIP] Failed to parse destination %s: %v", dest, err)
+		return
+	}
+
+	// Create a proxy request
+	proxyReq := req.Clone()
+	proxyReq.Recipient = destAddr // Update Request-URI
+	
+	// Start Call Tracking
 	callID := req.CallID().Value()
 	e.cc.StartCall(req.From().Address.String(), req.To().Address.String(), callID, tenantID)
 
+	// Send request through client and relay responses
+	ctx := context.Background()
+	clTx, err := e.server.TransactionRequest(ctx, proxyReq)
+	if err != nil {
+		log.Printf("[SIP] Proxy error: %v", err)
+		return
+	}
 
-	log.Printf("Routing INVITE to %s", dest)
-	// In reality, here we would use sipgo to forward the request
+	go func() {
+		defer clTx.Terminate()
+		for {
+			select {
+			case res := <-clTx.Responses():
+				if res == nil {
+					return
+				}
+				// Relay response back to original caller
+				proxyRes := res.Clone()
+				// Note: In a full proxy, we would adjust headers here
+				tx.Respond(proxyRes)
+			case <-tx.Done():
+				return
+			case <-clTx.Done():
+				return
+			}
+		}
+	}()
 }
 
 func (e *SIPEngine) onRegister(req *sip.Request, tx sip.ServerTransaction) {
@@ -104,9 +143,38 @@ func (e *SIPEngine) onBye(req *sip.Request, tx sip.ServerTransaction) {
 func (e *SIPEngine) onMessage(req *sip.Request, tx sip.ServerTransaction) {
 	log.Printf("[SIP] MESSAGE received from %s to %s", req.From().Address.String(), req.To().Address.String())
 	
-	// Simply acknowledge for now to stop "pending" status on clients
-	res := sip.NewResponseFromRequest(req, 200, "OK", nil)
-	tx.Respond(res)
+	dest, err := e.router.Route(req)
+	if err != nil {
+		res := sip.NewResponseFromRequest(req, 404, "Not Found", nil)
+		tx.Respond(res)
+		return
+	}
+
+	destAddr, _ := sip.ParseAddrUri(dest)
+	proxyReq := req.Clone()
+	proxyReq.Recipient = destAddr
+
+	ctx := context.Background()
+	clTx, err := e.server.TransactionRequest(ctx, proxyReq)
+	if err != nil {
+		res := sip.NewResponseFromRequest(req, 500, "Server Internal Error", nil)
+		tx.Respond(res)
+		return
+	}
+
+	go func() {
+		defer clTx.Terminate()
+		select {
+		case res := <-clTx.Responses():
+			if res != nil {
+				tx.Respond(res.Clone())
+			}
+		case <-time.After(5 * time.Second):
+			res := sip.NewResponseFromRequest(req, 408, "Request Timeout", nil)
+			tx.Respond(res)
+		}
+	}()
 }
+
 
 
